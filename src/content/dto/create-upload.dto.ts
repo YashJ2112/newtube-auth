5bbbd94fad437291c692852705a635e68a7c7ab2
YashJagwani <yashjagwani2112@gmail.com>
2022-05-20 16:37:18 +0200

    removed old auth

--- src/content/dto/create-upload.dto.ts
+++
@@ -1,7 +0,0 @@
-export class CreateUploadDTO {
-  readonly title: string;
-  readonly description: string;
-  readonly body: string;
-  readonly author: string;
-  readonly date_posted: string;
-}
--- src/content/schemas/content.schema.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from 'mongoose';
-
-export const ContentSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  body: String,
-  author: String,
-  date_posted: String,
-});
